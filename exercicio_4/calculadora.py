99a9dfb7fd4868bd15689c4eba7358aee3506397
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-07 16:59:39 -0300

    Criado tratamento de excecoes para o exercicio 4

--- exercicio_4/calculadora.py
+++ exercicio_4/calculadora.py
@@ -1,7 +1,31 @@
 try:
-    1 / 0
-except Exception as err:
-    # Tratamento caso no bloco do try acontece alguma exceção
-    # Outros tipos de exceções: https://docs.python.org/3/library/exceptions.html#exception-hierarchy
-    print("Divisão por zero")
+    firstValue = int(input("First value: "))
+    secondValue = int(input("Second value: "))
+except Exception as exception:
+    print("Invalid number")
     exit(1)
+
+operator = input("Operator: ")
+
+result = float
+
+OPERATOR_SUM = "+"
+OPERATOR_SUBTRACTION = "-"
+OPERATOR_MULTIPLICATION = "*"
+OPERATOR_DIVISION = "/"
+
+#Main
+
+if operator == OPERATOR_SUM:
+    result = firstValue + secondValue
+elif operator == OPERATOR_SUBTRACTION:
+    result = firstValue - secondValue
+elif operator == OPERATOR_MULTIPLICATION:
+    result = firstValue * secondValue
+elif operator == OPERATOR_DIVISION:
+    result = firstValue / secondValue
+else:
+    print("Invalid option!")
+    quit(1)
+
+print(f"{firstValue} {operator} {secondValue} = {result}")
