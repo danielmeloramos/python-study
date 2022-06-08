9f12a62bd619d07376ce187ce4088f8443928462
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-08 09:29:38 -0300

    Modificado a versao do Flask, para ultima versao. Removido comentarios.

--- exercicio_6/hello_world.py
+++ exercicio_6/hello_world.py
@@ -1,16 +1,7 @@
-# import Flask
 from flask import Flask
 
-
-# Configuração do Flask
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET"])
 def hello():
-    """Rota
-
-    Através do decorator app.route é definido uma rota `/`
-    que aceita o método GET e retorna o texto `Hello, World`
-    """
     return "Hello, World"
