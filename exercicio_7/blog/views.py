aee5fdc228127bb83395d8a77f379dfe98ddc9de
Daniel Melo Ramos <meloramosdaniel@gmail.com>
2022-06-08 09:47:23 -0300

    Atualizado para ultima versao do Flask. Removido comentarios; Atualizado conteudo pra compreender funcionamento. Exercicio 7.

--- exercicio_7/blog/views.py
+++ exercicio_7/blog/views.py
@@ -6,10 +6,4 @@ from blog import app
 
 @app.route("/", methods=["GET"])
 def blog():
-    """Blog
-
-    Registrando uma rota `/` que aceita GET e renderiza o template
-    `blog.html` e passa para a variável `content` do template o valor
-    `Hello, World`
-    """
-    return render_template("blog.html", content="Hello, World")
+    return render_template("blog.html", content="Daniel Melo Ramos")
--- exercicio_7/tests/views_test.py
+++ exercicio_7/tests/views_test.py
@@ -2,17 +2,10 @@ import unittest
 
 from blog import app
 
-
 class TestViews(unittest.TestCase):
     def setUp(self):
-        """Cria uma app falsa do flask"""
         self.app = app.test_client()
 
     def test_blog_view(self):
-        """Test da rota principal
-
-        Simula uma requisição para a rota `/` com o método GET e
-        verifica se o conteúdo está dentro do resultado
-        """
         result = self.app.get("/")
-        self.assertIn("Hello, World", result.data.decode("utf-8"))
+        self.assertIn("Daniel Melo Ramos", result.data.decode("utf-8"))
